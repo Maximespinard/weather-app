e027d3f5190f8c0cb6db7cc080c1fbdeb14f625d
mspinard <mspinard@vulog.com>
2021-06-23 12:00:22 +0200

    setup project

---
+++ src/components/App/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Typography } from '@material-ui/core';
+
+const App = () => (
+  <div>
+    <Typography variant="h1" component="h2">
+      h1. Heading
+    </Typography>
+  </div>
+);
+
+export default App;
